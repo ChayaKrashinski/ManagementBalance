fe09796a26fab3c22f240bc10fa503932f8e57cb
chaya krashinski <a0527651529@gmail.com>
2024-05-15 21:55:31 +0300

    register route

---
+++ app/routes/user_routes.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import APIRouter,HTTPException
+
+from app.models.user import User
+
+user_router = APIRouter()
+
+@user_router.post("/register/")
+async def register(user: User):
+    return "hello you! "+user.name
---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+
+from app.routes.user_routes import user_router
+
+app = FastAPI()
+app.include_router(router=user_router, prefix='/user')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
